2210445e58d071fa9721ed44b57e023ce3882688
HWI <ming037@naver.com>
2017-08-21 18:18:12 +0900

    tt

--- MyBusStop.py
+++ MyBusStop.py
@@ -6,11 +6,118 @@ import schedule
 import time
 import requests
 from bs4 import BeautifulSoup
-from PyQt5.QtWidgets import (QWidget, QLabel, QApplication)
-from PyQt5.QtCore import Qt, QSize
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 
-class Parsing(threading.Thread):
+
+class TestThread(QThread):
+    # 쓰레드의 커스텀 이벤트
+    # 데이터 전달 시 형을 명시해야 함
+    threadEvent = pyqtSignal(int)
+
+    def __init__(self, parent=None):
+        super().__init__()
+        self.n = 0
+        self.main = parent
+        self.isRun = False
+
+    def run(self):
+        while self.isRun:
+            print('쓰레드 : ' + str(self.n))
+
+            # 'threadEvent' 이벤트 발생
+            # 파라미터 전달 가능(객체도 가능)
+            self.threadEvent.emit(self.n)
+
+            self.n += 1
+            self.sleep(1)
+
+
+class Parsing(QObject):
+    threadEvent = pyqtSignal(int)
+
+    def __init__(self, interval, getClass):
+        super().__init__()
+
+        self.interval = interval
+        self.getClass = getClass
+        self.end = True  # 스레드 관리용 (종료시 스레드 멈추기)
+
+
+
+    @pyqtSlot()
+    def run(self):
+        url = 'http://openapi.jeonju.go.kr/jeonjubus/openApi/traffic/bus_location_busstop_infomation.do'
+        key = ''
+        stop_id_1 = '306101044'  # [31043] 전북대학교(농협앞)
+        stop_id_2 = '306101049'  # [31048] 전북대학교(한나여성병원앞)
+        self.get_bus_data(url, key, stop_id_1, 1)
+        self.get_bus_data(url, key, stop_id_2, 2)
+        schedule.every(self.interval).seconds.do(self.get_bus_data, url, key, stop_id_1, 1)
+        schedule.every(self.interval).seconds.do(self.get_bus_data, url, key, stop_id_2, 2)
+
+
+        while self.end:
+            schedule.run_pending()
+            time.sleep(1)
+
+    def get_bus_data(self, url, key, stop_id, position):
+        line = []
+        arrival = []
+        current_pass = []
+        heading = []
+        soon = []
+        soon_index = 0
+        result_numbers = []  # 결과 버스 노선 번호
+        result_waiting = []  # 결과 대기시간
+        result_current = []  # 결과 현재 위치
+        result_heading =[]  # 결과 행선지
+        soon_result = "DNPJMXRLEORSXPM"
+        source_code = requests.post(url, data={'authApiKey': key, 'stopStdid': stop_id, "orderBy":"rTime", "range":"asc"})
+        #http://openapi.jeonju.go.kr/jeonjubus/openApi/traffic/bus_location_busstop_infomation.do?authApiKey=DNPJMXRLEORSXPM&stopStdid=306101049&orderBy=rTime&range=asc
+        plain_text = source_code.text
+        soup = BeautifulSoup(plain_text, 'xml')  # source code from the website
+        #  노선:6 분선:4 도착시간:2 현재위치:15 방면:8
+        for link in soup.findAll("list"):
+            # 노선정보
+            get_line_info = link.contents[6].text
+            get_subline_info = link.contents[4].text
+            if get_subline_info != '0':
+                get_line_info += '-'
+                get_line_info += get_subline_info
+            line.append(get_line_info)
+            # 남은 시간 정보
+            get_time_info = int(link.contents[2].text) // 60
+            arrival.append(str(get_time_info)+"분")
+            if get_time_info <= 3:
+                soon.append(soon_index)
+            soon_index += 1
+            # 현재 위치 정보
+            current_pass.append(link.contents[15].text)
+            # 방면 정보
+            get_heading_info = link.contents[8].text
+            heading.append(get_heading_info[get_heading_info.find('>')+1:])
+
+        # Set ResultData
+        for index in range(0, len(line)):
+            if index not in soon:
+                result_numbers.append(line[index])
+                result_waiting.append(arrival[index])
+                result_current.append(current_pass[index])
+                result_heading.append(heading[index])
+
+        for index in range(0, len(soon)):
+            soon_result += line[index]
+            soon_result += ", "
+
+        self.print_info(result_numbers,result_waiting,result_current, result_heading, soon_result, position)
+
+
+
+
+
+class Parsing2(threading.Thread):
     def __init__(self, interval, getClass):
         super().__init__()
         self.interval = interval
